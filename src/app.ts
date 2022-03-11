0269e8a88ed51674aca81f4ca564e24172f37bd4
jsloat <john@ardoq.com>
2022-03-11 11:04:11 -0800

    Datastore testing

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,40 @@ import "core-js/stable";
 import "regenerator-runtime/runtime";
 import Fastify from "fastify";
 import Helmet from "fastify-helmet";
+import { Datastore } from "@google-cloud/datastore";
 
 const app = Fastify({ logger: true });
+
+const datastore = new Datastore();
+
+const quickstart = async () => {
+  // The kind for the new entity
+  const kind = "Task";
+  // The name/ID for the new entity
+  const name = "sampletask1";
+  // The Cloud Datastore key for the new entity
+  const taskKey = datastore.key([kind, name]);
+  // Prepares the new entity
+  const task = {
+    key: taskKey,
+    data: {
+      description: "Buy milk",
+    },
+  };
+  // Saves the entity
+  await datastore.save(task);
+  console.log(`Saved ${task.key.name}: ${task.data.description}`);
+};
 
 app.register(Helmet);
 
 app.get("/", (request, reply) => {
   reply.send({ hello: "world" });
+});
+
+app.get("/datastore-test", async (_, reply) => {
+  await quickstart();
+  reply.send({ status: "SUCCESS!!" });
 });
 
 const start = async () => {
